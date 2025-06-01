e5234aa01f41d23d6477ae3a3b312932212a71fb
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-01 12:58:23 +0200

    Erstellung Level 5 (01.06.2025)
    Erstellung Level 5. Anpassung Task-Script und SpeechBubbleExplain-Script.

---
+++ Assets/Scripts/Level 1-8/Level 5/WMSTask.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class WMSTask : MonoBehaviour
+{
+    public MultiSelectButtons multiSelectButtons;
+    public Button buttonWMS;
+    public Button buttonNext;
+    public GameObject speechBubbleTrue;
+    public GameObject speechBubbleFalse;
+    public GameObject speechBubbleFalse2;
+
+    void Start()
+    {
+        if (buttonNext != null)
+        {
+            buttonNext.onClick.AddListener(OnNextClicked);
+        }
+
+        if (speechBubbleTrue != null) speechBubbleTrue.SetActive(false);
+        if (speechBubbleFalse != null) speechBubbleFalse.SetActive(false);
+        if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(false);
+    }
+
+    void OnNextClicked()
+    {
+        if (multiSelectButtons == null || buttonWMS == null)
+            return;
+
+        List<Button> selected = multiSelectButtons.GetSelectedButtons();
+
+        bool isOnlyWMSSelected = (selected.Count == 1 && selected.Contains(buttonWMS));
+
+        if (speechBubbleTrue != null) speechBubbleTrue.SetActive(false);
+        if (speechBubbleFalse != null) speechBubbleFalse.SetActive(false);
+        if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(false);
+
+        if (isOnlyWMSSelected)
+        {
+            if (speechBubbleTrue != null) speechBubbleTrue.SetActive(true);
+        }
+        else
+        {
+            string currentLevel = SceneManager.GetActiveScene().name;
+            bool restarted = LevelRestartTracker.WasLevelRestarted(currentLevel);
+
+            if (restarted)
+            {
+                if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(true);
+            }
+            else
+            {
+                if (speechBubbleFalse != null) speechBubbleFalse.SetActive(true);
+            }
+        }
+    }
+}
