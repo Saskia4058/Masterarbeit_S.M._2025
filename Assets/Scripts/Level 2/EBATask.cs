e6d1a25a6d16e2e9863dcf458f41acbdba81eb12
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-01 10:49:10 +0200

    Erw. Level 2 mit neuer Restart-Logik (01.06.2025)
    Erweiterung Level 2 mit neuer Restart-Logik (Integration SpeechBubbleFalse2, neues Script RestartLevels an GameObject RestartLevel gehängt, Script EBATask angepasst). Script RestartLevel2 & 3 gelöscht (da nicht mehr notwendig).

--- Assets/Scripts/Level 2/EBATask.cs
+++ Assets/Scripts/Level 2/EBATask.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class EBATask : MonoBehaviour
 {
     public MultiSelectButtons multiSelectButtons;
+
+    public Button buttonEFZ;
     public Button buttonEBA;
     public Button buttonNext;
 
     public GameObject speechBubbleTrue;
     public GameObject speechBubbleFalse;
+    public GameObject speechBubbleFalse2;
 
     void Start()
     {
@@ -24,23 +28,40 @@ public class EBATask : MonoBehaviour
 
     void OnNextClicked()
     {
-        if (multiSelectButtons == null || buttonEBA == null)
+        if (multiSelectButtons == null)
             return;
 
         List<Button> selected = multiSelectButtons.GetSelectedButtons();
 
-        bool isOnlyEFZSelected = (selected.Count == 1 && selected.Contains(buttonEBA));
+        bool containsEFZ = selected.Contains(buttonEFZ);
+        bool containsEBA = selected.Contains(buttonEBA);
+
+        bool isExactlyTheseTwoSelected = 
+            selected.Count == 2 &&
+            containsEFZ && containsEBA;
 
         if (speechBubbleTrue != null) speechBubbleTrue.SetActive(false);
         if (speechBubbleFalse != null) speechBubbleFalse.SetActive(false);
+        if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(false);
 
-        if (isOnlyEFZSelected)
+        if (isExactlyTheseTwoSelected)
         {
             if (speechBubbleTrue != null) speechBubbleTrue.SetActive(true);
         }
         else
         {
-            if (speechBubbleFalse != null) speechBubbleFalse.SetActive(true);
+            string currentLevel = SceneManager.GetActiveScene().name;
+
+            bool restarted = LevelRestartTracker.WasLevelRestarted(currentLevel);
+
+            if (restarted)
+            {
+                if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(true);
+            }
+            else
+            {
+                if (speechBubbleFalse != null) speechBubbleFalse.SetActive(true);
+            }
         }
     }
 }
