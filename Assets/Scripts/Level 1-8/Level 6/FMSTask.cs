840c245b8c1732dbb2e3add5cabb0d88efe42a12
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-01 13:24:18 +0200

    Erstellung Level 6 (01.06.2025)
    Erstellung Level 6. Anpassung Task-Script und SpeechBubbleExplain-Script.

---
+++ Assets/Scripts/Level 1-8/Level 6/FMSTask.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class FMSTask : MonoBehaviour
+{
+    public MultiSelectButtons multiSelectButtons;
+
+    public Button buttonFMS;
+    public Button buttonFaMa;
+    public Button buttonFH;
+    public Button buttonNext;
+
+    public GameObject speechBubbleTrue;
+    public GameObject speechBubbleFalse;
+    public GameObject speechBubbleFalse2;
+
+    void Start()
+    {
+        if (buttonNext != null)
+        {
+            buttonNext.onClick.AddListener(OnNextClicked);
+        }
+
+        if (speechBubbleTrue != null) speechBubbleTrue.SetActive(false);
+        if (speechBubbleFalse != null) speechBubbleFalse.SetActive(false);
+    }
+
+    void OnNextClicked()
+    {
+        if (multiSelectButtons == null)
+            return;
+
+        List<Button> selected = multiSelectButtons.GetSelectedButtons();
+
+        bool containsFMS = selected.Contains(buttonFMS);
+        bool containsFaMa = selected.Contains(buttonFaMa);
+        bool containsFH = selected.Contains(buttonFH);
+
+        bool isExactlyTheseThreeSelected = 
+            selected.Count == 3 &&
+            containsFMS && containsFaMa && containsFH;
+
+        if (speechBubbleTrue != null) speechBubbleTrue.SetActive(false);
+        if (speechBubbleFalse != null) speechBubbleFalse.SetActive(false);
+        if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(false);
+
+        if (isExactlyTheseThreeSelected)
+        {
+            if (speechBubbleTrue != null) speechBubbleTrue.SetActive(true);
+        }
+        else
+        {
+            string currentLevel = SceneManager.GetActiveScene().name;
+
+            bool restarted = LevelRestartTracker.WasLevelRestarted(currentLevel);
+
+            if (restarted)
+            {
+                if (speechBubbleFalse2 != null) speechBubbleFalse2.SetActive(true);
+            }
+            else
+            {
+                if (speechBubbleFalse != null) speechBubbleFalse.SetActive(true);
+            }
+        }
+    }
+}
