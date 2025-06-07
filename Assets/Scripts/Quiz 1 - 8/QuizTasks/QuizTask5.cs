39051d24a08568e1d0be71fab7aadd7a43e5d4bd
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-07 10:24:03 +0200

    Lösungsanzeige Quiz 5 implementiert (07.06.2025)
    In Scene neues GameObject "SolutionRevealController" erstellt und Script "QuizSolutionRevealController"dran gehängt. Wo nötig, Zuweisungen im Inspector gemacht. QuizTask5-Script ergänzt.

--- Assets/Scripts/Quiz 1 - 8/QuizTasks/QuizTask5.cs
+++ Assets/Scripts/Quiz 1 - 8/QuizTasks/QuizTask5.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class QuizTask5 : MonoBehaviour
+public class QuizTask5 : MonoBehaviour, ILevelTask
 {
     public QuizSelectButtons quizSelectButtons;
     public Button buttonAnswer4;
@@ -42,5 +42,9 @@ public class QuizTask5 : MonoBehaviour
         {
             if (speechBubbleFalse != null) speechBubbleFalse.SetActive(true);
         }
+    }
+        public List<Button> GetCorrectButtons()
+    {
+        return new List<Button> { buttonAnswer4 };
     }
 }
