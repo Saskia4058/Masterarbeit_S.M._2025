352e6b9760806674422ecc36d8b5a1649846363e
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-09 13:58:44 +0200

    Neues Script ButtonStateController in Level 1 angehängt (09.06.2025)
    Neues Script erstellt, damit die Buttons/Elemente des Bildungssystems bei gewissen Zuständen nicht anklickbar sind. Script an GameObject ButtonManager gehängt.

---
+++ Assets/Scripts/ButtonStateController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ButtonStateController : MonoBehaviour
+{
+    private Button replayButton;
+    private GameObject speechBubbles;
+
+    private readonly HashSet<string> excludedButtons = new()
+    {
+        "ButtonWeiter", "ButtonHome", "ButtonYes", "ButtonNo"
+    };
+
+    void Start()
+    {
+        speechBubbles = GameObject.Find("Content/SpeechBubbles");
+        if (speechBubbles == null) return;
+
+        Transform sbExercise = speechBubbles.transform.Find("SpeechBubbleExercise");
+        if (sbExercise != null)
+        {
+            Transform rb = sbExercise.Find("ButtonReplay");
+            if (rb != null)
+            {
+                replayButton = rb.GetComponent<Button>();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (replayButton == null || speechBubbles == null) return;
+
+        bool isReplayActive = replayButton.gameObject.activeInHierarchy;
+
+        bool isAnyBubbleActive =
+            speechBubbles.transform.Find("SpeechBubbleTrue")?.gameObject.activeInHierarchy == true ||
+            speechBubbles.transform.Find("SpeechBubbleFalse")?.gameObject.activeInHierarchy == true ||
+            speechBubbles.transform.Find("SpeechBubbleFalse2")?.gameObject.activeInHierarchy == true ||
+            speechBubbles.transform.Find("SpeechBubbleAgain")?.gameObject.activeInHierarchy == true;
+
+        bool shouldBeInteractable = isReplayActive && !isAnyBubbleActive;
+
+        foreach (Button btn in FindObjectsOfType<Button>())
+        {
+            if (excludedButtons.Contains(btn.name))
+                continue;
+
+            btn.interactable = shouldBeInteractable;
+        }
+    }
+}
