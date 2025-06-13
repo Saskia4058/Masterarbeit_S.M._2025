be2b0464b04f926eeb9ed7d5d23df54e4d13318a
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-13 09:51:56 +0200

    Klickgeräusch ButtonHome eingefügt (13.06.2025)
    Mit Hilfe eines Scripts, ein Klickgeräusch beim ButtonHome eingefügt. Scriptname ist ButtonHomeFunction.

---
+++ Assets/Scripts/ButtonHomeFunction.cs
@@ -0,0 +1,33 @@
+// Urheber Soundeffekt (Klickgeräusch):
+// Sound Effect by <a href="https://pixabay.com/de/users/u_8g40a9z0la-45586904/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=234708">u_8g40a9z0la</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=234708">Pixabay</a> 
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(AudioSource))]
+public class ButtonHomeFunction : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private Button button;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(PlayClickSound);
+    }
+
+    void PlayClickSound()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource oder AudioClip fehlt auf ButtonHome.");
+        }
+    }
+}
