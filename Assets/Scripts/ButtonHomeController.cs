6638d6fd4c698ea151b62974cd14c4743fbc3052
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-16 18:28:04 +0200

    Anpassung Funktion ButtonHome (16.06.2025)
    Script ButtonHomeFunction gelöscht. Neues Script ButtonHomeController erstellt. Button Home überarbeitet. Image PauseInteractionBlocker eingefügt (stretch, überall 0).

---
+++ Assets/Scripts/ButtonHomeController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHomeController : MonoBehaviour
+{
+    private bool isGamePaused = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+        }
+        if (!isGamePaused)
+        {
+            Time.timeScale = 1f;
+        }
+    }
+    public void ToogelGamePaused()
+    {
+        isGamePaused = !isGamePaused;
+    }
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene("Startscreen");
+    }
+}
