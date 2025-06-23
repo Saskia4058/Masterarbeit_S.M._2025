e2e9101ffa3d5779e0c7e6c4b7bff04cd7c0cc65
Saskia4058 <saskia.mueller@stud.fhgr.ch>
2025-06-23 10:42:47 +0200

    Image bei ExerciseInteractionBlocker eingefügt (23.06.2025)
    Beim ExerciseInteractionBlocker ein Image eingefügt und mittels Animation-Funktion pulsieren lassen (alle Levels).

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -1,3 +1,6 @@
+//Music by 
+//< a href="https://pixabay.com/users/lnplusmusic-47631836/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=340445">Andrii Poradovskyi</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=340445">Pixabay</a>
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
